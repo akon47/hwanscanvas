a9ff2718c3ea87c9a5291a0c95556296d1f6378e
Kim Hwan <akon47@naver.com>
2021-07-23 15:51:03 +0900

    Grid, Flex 이용하여 레이아웃 형태 잡음, 스크롤바 스타일링

---
+++ src/components/editor/EditorCanvas.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="editor-canvas-container">
+    <div class="tools">tools</div>
+    <div class="canvas-container">
+      <div class="canvas">canvas</div>
+      <div calss="status-bar">status-bar</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "EditorCanvas",
+});
+</script>
+
+<style scoped>
+.editor-canvas-container {
+  display: grid;
+  grid-template-columns: 50px 1fr;
+  grid-template-rows: 1fr;
+
+  width: 100%;
+  height: 100%;
+}
+
+.editor-canvas-container > .tools {
+  grid-column: 1;
+  grid-row: 1;
+  background: #212121;
+}
+
+.editor-canvas-container > .canvas-container {
+  grid-column: 2;
+  grid-row: 1;
+
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr 30px;
+}
+
+.canvas-container > .canvas {
+  grid-column: 1;
+  grid-row: 1;
+
+  overflow: scroll;
+}
+
+.canvas-container > .status-bar {
+  grid-column: 1;
+  grid-row: 2;
+  background: #878787;
+}
+</style>
--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -1,20 +1,56 @@
 <template>
-  <div class="editor">
-    <slide-editor></slide-editor>
+  <div class="editor-container">
+    <div class="editor-canvas">
+      <editor-canvas></editor-canvas>
+    </div>
+    <div class="editor-tools">tools</div>
+    <div class="editor-timeline">timeline</div>
   </div>
 </template>
 
 <script lang="ts">
-import SlideEditor from "@/components/editor/SlideEditor.vue";
+import EditorCanvas from "@/components/editor/EditorCanvas.vue";
 import { defineComponent } from "vue";
 
 export default defineComponent({
+  components: { EditorCanvas },
   name: "Editor",
-  components: { SlideEditor },
 });
 </script>
 <style scoped>
-.editor {
-  overflow: visible;
+.editor-container {
+  display: grid;
+  grid-template-columns: 1fr 300px;
+  grid-template-rows: 1fr 150px;
+
+  width: 100%;
+  height: 100%;
+}
+
+.editor-container > div {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+
+.editor-container > .editor-canvas {
+  grid-column: 1;
+  grid-row: 1;
+}
+
+.editor-container > .editor-tools {
+  grid-column: 2;
+  grid-row: 1 / span 2;
+
+  background: chocolate;
+  overflow-y: auto;
+}
+
+.editor-container > .editor-timeline {
+  grid-column: 1;
+  grid-row: 2;
+
+  background: cornflowerblue;
+  overflow-y: auto;
 }
 </style>
